c4ef9f7ba939ced8a64f44e6550d4dab1e55a4e5
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-03-07 21:37:48 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,16 @@ import './Game.css'
 import { GAME_STATE_PLAYING } from '../Constant'
 
 function Footer({onNewGameClick, onSuggestClick, gameState}) {
+
+    const renderButton = () => {
+        if(gameState !== GAME_STATE_PLAYING){
+            return <button onClick={onNewGameClick}>New Game</button>
+        }
+        return <button onClick={onSuggestClick}>Suggest</button>
+    }
   return (
     <div className='panel footer'>
-        {gameState !== GAME_STATE_PLAYING &&
-        <button onClick={onNewGameClick}>New Game</button>
-        }
-        {gameState === GAME_STATE_PLAYING && <button onClick={onSuggestClick}>Suggest</button>}
-        
+      {renderButton()}
     </div>
   )
 }
