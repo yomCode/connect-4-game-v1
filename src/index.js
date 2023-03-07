e65ff85d2ba4143584a8f39cdd6a98df04160222
Abayomi Mustapha <mustaphaabayomi71@gmail.com>
2023-03-07 15:18:17 +0100

    update with app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GameBoard from './components/GameBoard';
+import App from './screen/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <GameBoard />
+    <App />
 );
---
+++ src/screen/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameBoard from '../components/GameBoard'
+
+function App() {
+  return (
+    <div className='app-container'>
+        <GameBoard />
+    </div>
+  )
+}
+
+export default App
